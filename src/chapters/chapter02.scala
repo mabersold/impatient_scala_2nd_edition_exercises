a0153ebf5eb2f60083778ada88e10f588fe1b1f3
Mark Abersold <mabersold@landmine.sea.marchex.com>
2017-01-24 09:07:37 -0800

    Added solution for chapter 2, exercise 11

--- src/chapters/chapter02.scala
+++ src/chapters/chapter02.scala
@@ -1,5 +1,7 @@
 package chapters
 
+import java.time.LocalDate
+
 object Chapter02 {
   def runExercises {
     println("-------------Beginning Chapter 02 Exercises------------------")
@@ -12,6 +14,7 @@ object Chapter02 {
     exercise8
     exercise9
     exercise10
+    exercise11
     println("*************Ending Chapter 02 Exercises*****************\n")
   }
 
@@ -108,6 +111,31 @@ object Chapter02 {
     println(f"The value of 2^2 is ${pow(2, 2)}")
     println(f"The value of 3^3 is ${pow(3, 3)}")
     println(f"The value of 4^-2 is ${pow(4, -2)}")
+  }
+
+  //11. Date interpolator
+  def exercise11: Unit = {
+
+    val year = 2000
+    val month = 6
+    val day = 11
+
+    val x: LocalDate = date"$year-$month-$day"
+    println(s"${x.getMonth.name()} ${x.getDayOfMonth}, ${x.getYear} was a ${x.getDayOfWeek}")
+  }
+
+  //This could be improved
+  implicit class DateInterpolator(val sc: StringContext) extends AnyVal {
+    def date(args: Any*): LocalDate = {
+      if(args.size != 3)
+        throw new IllegalArgumentException("Must pass three values")
+      else if (sc.parts.filter(part => part != "-" && part != "").size > 0)
+        throw new IllegalArgumentException("Must be separated by dashes")
+      else if (args.filterNot(arg => arg.isInstanceOf[Integer]).size > 0)
+        throw new IllegalArgumentException("All values must be integers")
+      else
+        LocalDate.of(args(0).toString.toInt, args(1).toString.toInt, args(2).toString.toInt)
+    }
   }
 }
 
